cdbbbd3a25a8167e09a3d2a7ba9b3fdf03d80bcd
cbrou <daddymac@gmail.com>
2013-06-27 13:29:02 -0500

    Changed buttons on Edit and Show pages

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -10,11 +10,11 @@ module NavigationHelper
     def render_breadcrumbs
         # Build breadcrumbs along association when applicable
         navigation_add "Coordinators", coordinators_path if @coordinators || @coordinator
-        navigation_add @coordinator.name, coordinator_path(@coordinator) if defined?(@coordinator) && @coordinator.name
+        navigation_add @coordinator.name, coordinator_path(@coordinator) if defined?(@coordinator) && @coordinator.id
         navigation_add "Events", coordinator_events_path(@coordinator)  if @events || @event
-        navigation_add @event.name, coordinator_event_path(@event.coordinator, @event) if defined?(@event) && @event.name
+        navigation_add @event.name, coordinator_event_path(@event.coordinator, @event) if defined?(@event) && @event.id
         navigation_add "Teams", coordinator_event_teams_path(@coordinator, @event) if @teams || @team
-        navigation_add @team.name, coordinator_event_team_path(@coordinator, @event, @team) if defined?(@team) && @team.name
+        navigation_add @team.name, coordinator_event_team_path(@coordinator, @event, @team) if defined?(@team) && @team.id
 
         render :partial => 'shared/navigation', :locals => { :nav => ensure_navigation }
     end
--- app/models/coordinator.rb
+++ app/models/coordinator.rb
@@ -1,4 +1,5 @@
 class Coordinator < ActiveRecord::Base
   attr_accessible :about, :contact, :location, :name
   has_many :events
+  validates :name, presence: true
 end
