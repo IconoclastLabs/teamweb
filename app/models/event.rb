179a5991a45e8d2bea657159433da8b9893bd661
Gant Laborde <gantman@gmail.com>
2013-06-27 10:38:46 -0500

    Ran annotate

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id             :integer          not null, primary key
+#  name           :string(255)
+#  about          :string(255)
+#  coordinator_id :integer
+#  location       :string(255)
+#  start          :date
+#  end            :date
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
 class Event < ActiveRecord::Base
   belongs_to :coordinator
   attr_accessible :about, :end, :location, :name, :start
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: teams
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  event_id   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  rank       :decimal(, )
+#
+
 class Team < ActiveRecord::Base
   belongs_to :event
   attr_accessible :name
