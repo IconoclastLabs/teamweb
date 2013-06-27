bee4e1a61f48f1ae88a2b755f3732f5e0326d9d1
cbrou <daddymac@gmail.com>
2013-06-27 16:34:49 -0500

    Changed buttons on all pages to button groups and changed some formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def icon_tag(classname)
     raw "<i class='icon-#{classname} icon-white'></i> " #trailing space here is important
   end	
+
+  def black_icon(classname)
+    raw "<i class='icon-#{classname}'></i> " #trailing space here is important
+  end	
 end
