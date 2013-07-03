cf27d93779dab46c9ceb80a1ccd4ab132d7511b1
Gant Laborde <gantman@gmail.com>
2013-07-03 13:47:05 -0500

    small rails 4 removals

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,5 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Teamweb::Application.config.secret_token = 'fcd1e82663e56d419cd73759087d1859619510fc634c084e60f55025592e9443522212a16d59a7a43ce45df9bfd136080c98112967f9fb9d66abb985fbae9354'
 Teamweb::Application.config.secret_key_base = 'xxd1e82663e56d419cd73759087d1859619510fc634c084e60f55025592e9443522212a16d59a7a43ce45df9bfd136080c98112967f9fb9d66abb985fbae9354'
