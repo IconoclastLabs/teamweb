fada0ab9677c95bebc0e3171fe0af9fd20bcb99f
Gant <GantMan@gmail.com>
2013-07-16 19:56:38 -0500

    Fixing some tests

--- test/functional/teams_controller_test.rb
+++ test/functional/teams_controller_test.rb
@@ -12,13 +12,13 @@ class TeamsControllerTest < ActionController::TestCase
   end
 
   test "get index" do
-    get :index, coordinator_id: @team.event.coordinator_id, event_id: @team.event_id
+    get :index, coordinator_id @team.event.coordinator_group_id, event_id: @team.event_id
     assert_response :success
     assert_not_nil assigns(:teams)
   end
 
   test "certain index buttons require login" do
-    get :index, coordinator_id: @team.event.coordinator_id, event_id: @team.event_id
+    get :index, coordinator_id @team.event.coordinator_group_id, event_id: @team.event_id
     assert_select "a", {count: 0, text: "New Team"}, "Shouldn't have a new button"
     assert_select "a", {count: 0, text: "Edit"}, "Shouldn't have an edit button"
     assert_select "a", {count: 0, text: "Delete"}, "Shouldn't have a delete button"
@@ -26,80 +26,80 @@ class TeamsControllerTest < ActionController::TestCase
 
   test "certain index buttons show with login" do
     sign_in User.first
-    get :index, coordinator_id: @team.event.coordinator_id, event_id: @team.event_id
+    get :index, coordinator_id @team.event.coordinator_group_id, event_id: @team.event_id
     assert_select "a", "New Team", "Should have a new button"
     assert_select "a", "Edit", "Should have an edit button"
     assert_select "a", "Delete", "Should have a delete button"
   end
 
   test "require login to get new" do
-    get :new, coordinator_id: @team.event.coordinator_id, event_id: @team.event_id
+    get :new, coordinator_id @team.event.coordinator_group_id, event_id: @team.event_id
     assert_response :redirect #302
   end
 
   test "should get new" do
     sign_in User.first
-    get :new, coordinator_id: @team.event.coordinator_id, event_id: @team.event_id
+    get :new, coordinator_id @team.event.coordinator_group_id, event_id: @team.event_id
     assert_response :success
     assert_not_nil assigns(:team)
   end
 
   test "require login to create team" do
     assert_no_difference('Team.count') do
-      post :create, coordinator_id: @new_team.event.coordinator_id, event_id: @new_team.event_id, team: { name: @new_team.name }
+      post :create, coordinator_id @new_team.event.coordinator_group_id, event_id: @new_team.event_id, team: { name: @new_team.name }
     end
   end
 
   test "create team" do
     sign_in User.first
     assert_difference('Team.count') do
-      post :create, coordinator_id: @team.event.coordinator_id, event_id: @team.event_id, team: { name: @new_team.name }
+      post :create, coordinator_id @team.event.coordinator_group_id, event_id: @team.event_id, team: { name: @new_team.name }
     end
 
-    assert_redirected_to coordinator_event_team_path(@team.event.coordinator, @team.event, assigns(:team))
+    assert_redirected_to coordinator_event_team_path(@team.event.coordinator_group, @team.event, assigns(:team))
   end
 
   test "show team" do
-    get :show, id: @team, coordinator_id: @team.event.coordinator_id, event_id: @team.event_id
+    get :show, id: @team, coordinator_id @team.event.coordinator_group_id, event_id: @team.event_id
     assert_response :success
     assert_not_nil assigns(:team)
   end
 
   test "require login to get edit" do
-    get :edit, id: @team, coordinator_id: @team.event.coordinator_id, event_id: @team.event_id
+    get :edit, id: @team, coordinator_id @team.event.coordinator_group_id, event_id: @team.event_id
     assert_response :redirect #302
   end
 
   test "get edit" do
     sign_in User.first
-    get :edit, id: @team, coordinator_id: @team.event.coordinator_id, event_id: @team.event_id
+    get :edit, id: @team, coordinator_id @team.event.coordinator_group_id, event_id: @team.event_id
     assert_response :success
     assert_not_nil assigns(:team)
   end
 
   test "require login to update team" do
-    put :update, id: @team, team: { name: @team.name }, coordinator_id: @team.event.coordinator_id, event_id: @team.event_id
+    put :update, id: @team, team: { name: @team.name }, coordinator_id: @team.event.coordinator_group_id, event_id: @team.event_id
     assert_redirected_to user_session_path
   end
 
   test "update team" do
     sign_in User.first
-    put :update, id: @team, team: { name: @team.name }, coordinator_id: @team.event.coordinator_id, event_id: @team.event_id
-    assert_redirected_to coordinator_event_team_path(coordinator_id: @team.event.coordinator_id, event_id: @team.event_id)
+    put :update, id: @team, team: { name: @team.name }, coordinator_id @team.event.coordinator_group_id, event_id: @team.event_id
+    assert_redirected_to coordinator_event_team_path(coordinator_id @team.event.coordinator_group_id, event_id: @team.event_id)
   end
 
   test "require login to destroy team" do
     assert_no_difference('Team.count') do
-      delete :destroy, id: @team, coordinator_id: @team.event.coordinator_id, event_id: @team.event_id
+      delete :destroy, id: @team, coordinator_id @team.event.coordinator_group_id, event_id: @team.event_id
     end
   end
 
   test "destroy team" do
     sign_in User.first
     assert_difference('Team.count', -1) do
-      delete :destroy, id: @team, coordinator_id: @team.event.coordinator_id, event_id: @team.event_id
+      delete :destroy, id: @team, coordinator_id @team.event.coordinator_group_id, event_id: @team.event_id
     end
-    assert_redirected_to coordinator_event_teams_path(@team.event.coordinator, @team.event)
+    assert_redirected_to coordinator_event_teams_path(@team.event.coordinator_group, @team.event)
     assert_not_nil assigns(:team)
   end
 end
