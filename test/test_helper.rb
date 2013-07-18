ae6fd4a9eaaa4bd8558269c5ea3c8bf0fd2d9bc9
Gant <GantMan@gmail.com>
2013-07-17 23:36:02 -0500

    Modifying the way tests output to conserve space but optimize the info

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -20,7 +20,7 @@ Turn.config do |c|
 #  # :pretty   - new pretty reporter
 #  # :marshal  - dump output as YAML (normal run mode only)
 #  # :cue      - interactive testing
-  c.format  = :outline
+  c.format  = :progress
 #  # turn on invoke/execute tracing, enable full backtrace
 #  c.trace   = true
 #  # use humanized test names (works only with :outline format)
