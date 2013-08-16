06e9f5361eeda2524393d4b79e8996ea7c5a2369
Matt Garrison <matt@iconoclastlabs.com>
2013-08-16 14:25:03 -0500

    working on adding auth to the api

--- app/controllers/api/v1/organizations_controller.rb
+++ app/controllers/api/v1/organizations_controller.rb
@@ -2,6 +2,8 @@ module Api
   module V1
     class OrganizationsController < ApplicationController
       respond_to :json
+      before_filter :authenticate_user!
+      
       def index
         @organizations = Organization.order(:name)
       end
--- test/controllers/api/v1/organization_controller_test.rb
+++ test/controllers/api/v1/organization_controller_test.rb
@@ -3,18 +3,20 @@ require 'test_helper'
 class Api::V1::OrganizationsControllerTest < ActionController::TestCase
   include Devise::TestHelpers
 
-  # setup do
-  #   @organization = organizations(:coord_one)
-  # end
+  describe "#index.json" do
+    before do
+      get :index, format: :json
+      @organization = organizations(:coord_one)
+    end
 
-  # test "get index" do
-  #   get :index
-  #   assert_response :success
-  #   #assert_not_nil assigns(:organizations)
-  # end
-  it "lists all organizations" do
-    #get :index
-    assert true
+    it "lists all organizations" do
+      assigns(:organizations).wont_be_nil
+      assert_response :success
+    end
+
+    it "responds with parsable JSON" do
+      json_response = JSON.parse response.body
+      json_response.wont_be_nil
+    end
   end
-
 end
