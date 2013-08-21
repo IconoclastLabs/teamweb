eb8e61ecd625167c929e21d6ec552a24fa6bf018
Gant Laborde <gantman@gmail.com>
2013-08-21 15:08:34 -0500

    Consolidated model tests for teams

--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -21,6 +21,7 @@ class TeamTest < ActiveSupport::TestCase
     @event_two = events(:event_two)
     @season_one = seasons(:season_one)
     @season_two = seasons(:season_two)
+    @no_teams_season = seasons(:season_three)
   }
   let(:simple_team) {Team.new(name: 'TestName', max_members: 5)}
   it 'can create a new Team' do
@@ -74,6 +75,37 @@ class TeamTest < ActiveSupport::TestCase
       success = @team_one.add_team_member(@first_user, admin_flag: false)
       success.must_equal true
     end
+  end
+
+  it 'cannot exceed max_members' do
+    simple_team.max_members = 1
+    simple_team.save.must_equal true
+    simple_team.add_team_member(users(:one)).must_equal true
+    simple_team.add_team_member(users(:two)).wont_equal true
+    simple_team.members.size.must_equal 1
+  end
+  
+  it "Teams are invalid on Seasons that disallow teams" do
+    @no_teams_season.teams_allowed?.must_equal false
+    simple_team.season = @no_teams_season
+    simple_team.valid?.must_equal false
+  end
+
+  it 'Max Members can never be set greater than Event Max Team Size' do
+    simple_team.season = @season_one
+    @season_one.max_team_size = 5
+    simple_team.max_members = 5
+    simple_team.valid?.must_equal true
+    simple_team.max_members = 6
+    simple_team.valid?.must_equal false
+  end
+
+  it 'cannot exceed max_members' do
+    simple_team.max_members = 1
+    simple_team.save.must_equal true
+    simple_team.add_team_member(users(:one)).must_equal true
+    simple_team.add_team_member(users(:two)).wont_equal true
+    simple_team.members.size.must_equal 1
   end
 
 end
