6d95042aa9a5a6a918a813223cba6249d962e815
Matt Garrison <matt@iconoclastlabs.com>
2013-09-17 15:56:41 -0500

    fixed the rack:lock test issue

--- config/initializers/rack-rewrite.rb
+++ config/initializers/rack-rewrite.rb
@@ -1,4 +1,4 @@
-Teamweb::Application.config.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
+Teamweb::Application.config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
   #rewrite '/fonts',  '/assets'
   rewrite %r{/fonts/(.*)}, '/assets/$1'
 end
