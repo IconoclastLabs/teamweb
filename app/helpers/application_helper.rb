6a7eecf189f7f6bf9967e45eaa4adaaee25953df
Matt Garrison <matt@iconoclastlabs.com>
2013-09-24 12:13:43 -0500

    updated tests for application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,19 @@
 module ApplicationHelper
   def fa_icon_tag(classname)
     raw "<i class='icon-#{classname} icon-white'></i> " #trailing space here is important
-  end	
+  end
 
   def fa_black_icon_tag(classname)
     raw "<i class='icon-#{classname}'></i> " #trailing space here is important
-  end	
+  end
 
   def bs_icon_tag(classname)
     raw "<i class='glyphicon glyphicon-#{classname}'></i> " #trailing space here is important
-  end 
+  end
   alias :icon_tag :bs_icon_tag
 
   def bs_black_icon_tag(classname)
     raw "<i class='glyphicon glyphicon-#{classname}'></i> " #trailing space here is important
-  end 
+  end
   alias :black_icon_tag :bs_black_icon_tag
 end
--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,15 +2,39 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
 
-	describe "#icon_tag" do
+	describe "icon_tag method alias" do
 	  it "renders a white icon_tag for bootstrap" do
-	    icon_tag("play").must_match "<i class='icon-play icon-white'></i>"
+	    icon_tag("play").must_match "<i class='glyphicon glyphicon-play'></i>"
 	  end
 	end
 
-	describe "#black_icon" do
+	describe "black_icon method alias" do
     it "renders black icon_tag for bootstrap" do
-      black_icon("play").must_match "<i class='icon-play'></i>"
+      black_icon_tag("play").must_match "<i class='glyphicon glyphicon-play'></i>"
   	end
+  end
+
+  describe "bootstrap icon tag test" do
+    it "renders a white icon_tag for bootstrap" do
+      bs_icon_tag("play").must_match "<i class='glyphicon glyphicon-play'></i>"
+    end
+  end
+
+  describe "bootstrap black icons tag test" do
+    it "renders black icon_tag for bootstrap" do
+      bs_black_icon_tag("play").must_match "<i class='glyphicon glyphicon-play'></i>"
+    end
+  end
+
+  describe "Font-Awesome icon tag test" do
+    it "renders a white icon_tag for bootstrap" do
+      fa_icon_tag("play").must_match "<i class='icon-play icon-white'></i>"
+    end
+  end
+
+  describe "Font-Awesome black icon tag test" do
+    it "renders black icon_tag for bootstrap" do
+      fa_black_icon_tag("play").must_match "<i class='icon-play'></i>"
+    end
   end
 end
